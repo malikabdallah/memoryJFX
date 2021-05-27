032b453064d666dc20af03b942e306c8a514e196
malikabdallah <malikabdallah75019@gmail.com>
2021-05-27 13:26:48 +0200

    vue menu

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.stage.Stage;
+import vue.Menu;
+
 public class Controller {
+
+    final Stage laStageUnique;
+
+
+    private Menu menuView;
+
+    public Controller(Stage stage) {
+        laStageUnique = stage;
+
+
+    }
+
+    public void goToJeu() {
+        //jeu=Jeu.creerEtAfficher(this,laStageUnique);
+
+    }
 }
